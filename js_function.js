34d095c4e8b39c2c5b568e5e281bc38c4dff4b8d
jahidcp <programmingloverjahid@gmail.com>
2023-01-30 00:09:39 +0600

    some function example practice

---
+++ js_function.js
@@ -0,0 +1,65 @@
+// // Function Declaration
+
+// function startFan(){
+//     console.log("Stand Up");
+//     console.log("Walk towards the switch");
+//     console.log("Pres The switch");
+// }
+
+// // Call the Function 
+
+// startFan();
+// startFan();
+// startFan();
+
+
+// function BringAsset(amount){
+//     console.log("I will Buy Some Polkadot Coin");
+//     console.log(amount)
+//     console.log("For amount of 21 Coin");
+// }
+// BringAsset("100$");
+
+
+// function HoldingAsset(HoldingBNB){
+//     console.log("When I am Holding", HoldingBNB,"BNB Then exact Change My Decision for IEO");
+//     console.log("I will Participate Binance IEO");
+// }
+
+// var myHoldingBNB = 233;
+// HoldingAsset(myHoldingBNB);
+
+
+// function holdingAssets(ethereum, USDC){
+//     console.log("I will Donate My", ethereum,"and",USDC,"When i will gain some Payment from any project");
+//     console.log("I will Donate For Pelestine people");
+// }
+
+// var holingEthereum = "0.89eth";
+// var holdingUsdc = "1240$";
+// holdingAssets(holingEthereum, holdingUsdc);
+
+// return Function
+
+// function addToken(token){
+//     var tokenPrice = 15;
+//     var tokenQuantity = token / tokenPrice;
+//     return tokenQuantity;
+// }
+
+// var amountInvest = 1500;
+
+// var totalPrice = addToken(amountInvest);
+// console.log(totalPrice);
+
+function addBalance(CryptoCoin){
+    console.log(coinPrice)
+    var coinPrice = 12;
+    var totalBuyQuantity = CryptoCoin / coinPrice;
+    return totalBuyQuantity;
+}
+
+var amountExactBuy = 1900;
+var totalCoinInvestment = addBalance(amountExactBuy);
+// var totalCoinInvestments = totalCoinInvestment.toFixed(2);
+console.log(totalCoinInvestment)
